d211f46a3b5476c9d73d2cc651d7715c6c703c87
Will Watson <will.watson@neso.io>
2019-05-29 21:27:45 +0100

    Added: gp margin & sale price from gp

--- biz.go
+++ biz.go
@@ -4,13 +4,32 @@ import "fmt"
 
 func main() {
 
-	fmt.Println(breakEvenPoint(5000,700,300))
+	 var Expenses float64;
+	 Expenses = 9500
+
+	fmt.Print(" £ ",breakEvenPoint(Expenses,salePriceFromGrossProfit(0.5,1),1))
+	fmt.Print(" -")
+	fmt.Print(" £ ",breakEvenPoint(Expenses,salePriceFromGrossProfit(0.7,1),1))
 }
 
-func breakEvenPoint(fixedCosts, salePrice, variableCost int) int {
+func breakEvenPoint(FixedCosts, SalePrice, VariableCost float64) int {
 
-	var ContributionMargin = salePrice - variableCost
-	var BreakEvenPoint = fixedCosts / ContributionMargin
+	var ContributionMargin = SalePrice - VariableCost
+	var BreakEvenPoint = FixedCosts / ContributionMargin
 
 	return int(BreakEvenPoint)
 }
+
+func grossProfitMargin(Revenue, CostOfSale float64) float64 {
+
+	var GrossProfitMargin = (Revenue - CostOfSale) / Revenue
+
+	return float64(GrossProfitMargin)
+}
+
+func salePriceFromGrossProfit(GrossProfitMargin, CostOfSale float64) float64 {
+	var SalePrice = CostOfSale + (CostOfSale * GrossProfitMargin)
+
+	return float64(SalePrice)
+}
+
